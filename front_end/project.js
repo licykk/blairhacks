9da3ccc8b9e90a653b4df7aeead713d26bcc934b
Licykk <licykk@gmail.com>
2017-12-02 15:53:02 -0500

    changed

--- front_end/project.js
+++ front_end/project.js
@@ -3,3 +3,14 @@ toggle between hiding and showing the dropdown content */
 function Dropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
+
+function NeededGrade () {
+  /*var PointsInClass = document.getElementById("num").value;
+  var CurrentPoints = document.getElementById("dem").value;
+  var TargetGrade = document.getElementById("test").value;
+  var TestPoints = document.getElementById("target").value;
+  var NeededGrade;
+  NeededGrade = (TargetGrade*(PointsInClass+TestPoints)-CurrentPoints)/(100*TestPoints);*/
+  document.getElementById("needed").innerHTML = "Points needed on test:";
+  /*return NeededGrade;*/
+}
