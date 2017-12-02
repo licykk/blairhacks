413c5368899b4aa265d51f71c2e6fb70d1b3079b
Licykk <licykk@gmail.com>
2017-12-02 16:38:34 -0500

    MADE IT WURK GURRLLL

--- front_end/project.js
+++ front_end/project.js
@@ -5,12 +5,13 @@ function Dropdown() {
 }
 
 function NeededGrade () {
-  /*var PointsInClass = document.getElementById("num").value;
-  var CurrentPoints = document.getElementById("dem").value;
-  var TargetGrade = document.getElementById("test").value;
-  var TestPoints = document.getElementById("target").value;
-  var NeededGrade;
-  NeededGrade = (TargetGrade*(PointsInClass+TestPoints)-CurrentPoints)/(100*TestPoints);*/
-  document.getElementById("needed").innerHTML = "Points needed on test:";
+  var PointsInClass = parseInt(document.getElementById("num").value);
+  var CurrentPoints = parseInt(document.getElementById("den").value);
+  var TestPoints = parseInt(document.getElementById("test").value);
+  var TargetGrade = parseInt(document.getElementById("target").value);
+
+  TargetGrade = TargetGrade/100;
+  var NeededGrade = TargetGrade*(CurrentPoints+TestPoints)-PointsInClass;
+  document.getElementById("needed").innerHTML = "Points needed on test: " + NeededGrade;
   /*return NeededGrade;*/
 }
